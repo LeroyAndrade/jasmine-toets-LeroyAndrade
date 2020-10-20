e3b39f5ca45c9477b894373c60f9ec0fa403acdb
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-10-20 10:57:17 +0200

    opgave1

--- toets/opdracht 3/student.js
+++ toets/opdracht 3/student.js
@@ -12,6 +12,26 @@ function Student(naam, studentId) {
 let student = new Student('myName', '0001');
 
 // Hier moet je de variabele klas een waarde geven bijvoorbeeld MD2
-console.log(
-    /* Hier moet de variabele klas getoond worden */
-)
+student.klas="MD2";
+    
+/* Hier moet de variabele klas getoond worden */
+console.log(student.klas);
+
+
+
+
+
+/*
+a. Public en Private variabelen (1 punt)
+Pas de code aan in het JavaScript student.js aan zodat er geen public variabelen meer in staan en alle variabelen private variabelen zijn.
+Maak ook methods waarmee je de klas variabele een waarde geeft en kunt tonen.
+*/
+
+class MyCounter {
+    #obj = 20;
+    showCount() {
+        return this.#count;
+    }
+}
+let counter = new MyCounter();
+console.log(counter.showCount());
